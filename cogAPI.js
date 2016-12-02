26afc7eadaa5b5b2f57be607f0b38696747cdead
jb2025 <justin.burke@gmail.com>
2016-12-02 13:27:44 -0500

    code worked a couple times, now has stopped working without modification of code

--- cogAPI.js
+++ cogAPI.js
@@ -2,58 +2,41 @@
 // API URL : https://api.projectoxford.ai/emotion/v1.0/recognize?
 var fs = require('fs');
 var url = require('url');
-var image_original = 'local.jpg';
+var http = require('https');
+var util = require('util');
+
+var returnData = [];
 var rawImageData = '';
-// var decodedImage = '';
-// var binaryImage = '';
-// var decodedImage = '';
-// fs.readFile(image_original, function(err, original_data){
-//     // decodedImage = new Buffer(original_data, 'binary').toString('binary');
-//     decodedImage = original_data.toString('binary');
-//     console.log(decodedImage);
-//     jbCallback(decodedImage);
-//     // binaryImage = new Buffer(decodedImage,'binary');
-//     // console.log(binaryImage);
-//     // fs.writeFile('image_decoded.jpg', decodedImage, function(err) {});
-// });
+var decodedImage = '';
 
-fs.readFile('local.jpg', (err, data) =>{ 
-    if (err) throw err;
-    rawImageData = new Buffer(data.toString('base64'),'base64');
-    console.log(rawImageData);
-    jbCallback(data);
+
+fs.readFile('local.jpg', function(err, data){
+    var myArray = new Buffer(data); 
+    var longInt8View = new Uint8Array(myArray);
+        for (var i=0; i<data.length; i++){
+            longInt8View[i] = data[i];
+        } 
+    jbCallback(myArray);
 });
-
-// fs.readFile('local.jpg', function(err, data){
-//     var myArray = new ArrayBuffer(data.length);
-//     var longInt8View = new Uint8Array(myArray);
-//         for (var i=0; i< longInt8View.length; i++){
-//         longInt8View[i] = i % 255;
-//         }   // rawImageData = new Uint8Array(data);
-//     console.log(myArray);
-//     jbCallback(myArray);
-// });
-
 
 function jbCallback(e) {
     post_req.write(e);
     post_req.end();
-};
+}
 
 
 
-var http = require('https');
-var util = require('util');
+
 var parsedURL = url.parse("https://api.projectoxford.ai/emotion/v1.0/recognize");
-console.log(rawImageData);
 var options = {
     host: parsedURL.host,
     path: '/emotion/v1.0/recognize',
+    // processData: false,
     method: 'POST',
     headers: {
         'Content-Type': 'application/octet-stream',
         'Ocp-Apim-Subscription-Key': "e1c8d985f4e04a5aa7703cd3e8b0e38d",
-        'Content-Length': rawImageData.length,
+        // 'Content-Length': myArray.length,
     },
     // data:{
     //     mimeType: 'application/octet-stream',
@@ -63,11 +46,13 @@ var options = {
 
 var post_req = http.request(options, function(res) {
     console.log('STATUS: ' + res.statusCode);
-    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    // console.log('HEADERS: ' + JSON.stringify(res.headers));
 
     res.on('data', function (responseBody) {
+        // console.log(responseBody.toString('utf-8'));
         console.log('BODY: ' + util.inspect(responseBody, {showHidden: false, depth: null}));
-        console.log(responseBody);
+        // console.log(responseBody);
+        // console.log(JSON.stringify(responseBody));
         // Do your processing post async response
     });
 });
